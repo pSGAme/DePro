8047f13d1a4d02a71803f0f9f0b9287fbcd57194
user <kxchen@seu.edu.cn>
2025-04-15 10:20:30 +0800

    n

--- src/algos/depro/trainer.py
+++ src/algos/depro/trainer.py
@@ -2,7 +2,7 @@ import sys
 from tqdm import tqdm
 import os
 
-code_path = '/home/user/Code/DePro'  # e.g. '/home/username/ProS'
+code_path = '/home/user/Code/DePro_SIGIR'  # e.g. '/home/username/ProS'
 sys.path.append(code_path)
 sys.path.append(os.path.join(code_path, "src"))
 sys.path.append(os.path.join(code_path, "clip"))
